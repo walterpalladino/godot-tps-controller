c0074d0608ca700617e1afa00bbae0ec11fb7d85
Walter H. Palladino <wpalladino@gmail.com>
2025-08-27 20:39:53 -0300

    Update for pickable coin

--- assets/side-platformer/scripts/pickable-platform.gd
+++ assets/side-platformer/scripts/pickable-platform.gd
@@ -1,9 +1,12 @@
 extends Area3D
 
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+	animation_player.play("coin_floating")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
